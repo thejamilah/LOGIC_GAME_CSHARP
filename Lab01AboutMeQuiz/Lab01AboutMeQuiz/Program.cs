e5c0669957593aeae4259cd7515c1a36f4030974
thejamilah <jamilahmcw@gmail.com>
2018-10-19 13:51:45 -0700

    gitinit

---
+++ Lab01AboutMeQuiz/Lab01AboutMeQuiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01AboutMeQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("About Me");
+        }
+    }
+}
